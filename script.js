b938d6f233e8fa277336463adb3b3514675f9591
Jay <124905892+letsc0de1t@users.noreply.github.com>
2023-02-16 22:08:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var text = document.getElementById("inputText")
+var value = document.getElementById("value")
+
+const updateCount = () =>{
+     var count = text.value.length
+     value.innerText = count
+}
